fa45135f78032cf5fce3d0edef0d4e6035274657
Kenji Sato <mail@kenjisato.jp>
2023-08-28 16:45:34 +0900

    change to jsdelivr

--- juicedown/js/modal.js
+++ juicedown/js/modal.js
@@ -1,2 +1,2 @@
-$('head').append('<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jquery-modal/0.9.1/jquery.modal.min.css" />');
+$('head').append('<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/jquery-modal@0.9.2/jquery.modal.min.css" />');
 $('head').append('<link rel="stylesheet" href="https://kenjisato.github.io/resources/juicedown/css/modal.css" />');
